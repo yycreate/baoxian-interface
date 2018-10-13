af5052e909586cc9102577c6d96a63f50bd07a1a
yy <1677150733@qq.com>
2018-10-13 17:38:31 +0800

    添加静态文件配置

---
+++ src/main/java/com/yxkj/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package com.yxkj.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        if(!registry.hasMappingForPattern("/static/**")){
+            registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        }
+        super.addResourceHandlers(registry);
+    }
+	
+}
